ed3bfed1db47f70186a964b525443633caaff314
Anastasya <anasty22@gmail.com>
2023-01-26 22:53:15 +0200

    add styles modalEv

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -1,24 +1,38 @@
-import React, { useContext } from 'react'
-import GlobalContext from '../context/GlobalContext'
+import React, { useContext, useState } from "react";
+import GlobalContext from "../context/GlobalContext";
 
 function EventModal() {
-    const {setShowEventModal}=useContext(GlobalContext)
+  const [title, setTitle] = useState("");
+  const { setShowEventModal } = useContext(GlobalContext);
   return (
-    <div className='h-screen w-full fixed left-0 top-0 flex justify-center items-center'>
-        <form className="bg-white rounded-lg shadow-2xl w-1/4">
-            <header className='bg-gray-100 px-4 py-2 flex justify-between items-center'>
-            <span className="material-icons-outlined text-gray-400">
+    <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
+      <form className="bg-white rounded-lg shadow-2xl w-1/4">
+        <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
+          <span className="material-icons-outlined text-gray-400">
             drag_handle
           </span>
-                <button onClick={()=>setShowEventModal(false)}>
-                <span className="material-icons-outlined text-gray-400">
-close
-                </span>
-                </button>
-            </header>
-        </form>
+          <button onClick={() => setShowEventModal(false)}>
+            <span className="material-icons-outlined text-gray-400">close</span>
+          </button>
+        </header>
+        <div className="p-3">
+          <div className="grid grid-cols-1/5 items-end gap-y-7">
+            <div></div>
+            <input
+              type="text"
+              name="title"
+              placeholder="Add Title"
+              value={title}
+              className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-green-500"
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+             <span className="material-icons-outlined text-gray-400">schedule</span>
+          </div>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default EventModal
+export default EventModal;
